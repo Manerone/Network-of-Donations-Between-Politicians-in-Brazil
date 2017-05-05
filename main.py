3e26ea415513d98301cd42113b60cda343d39c77
Matheus Nerone <matheus.agio@hotmail.com>
2017-05-05 11:26:21 -0300

    wip

---
+++ main.py
@@ -0,0 +1,65 @@
+'''This script creates a social network using candidates as vertices and donations as edges
+'''
+import os
+from pyspark import SparkContext, SQLContext
+from pyspark.sql import Row
+
+CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+CANDIDATES_PATH = CURRENT_PATH + '/databases/consulta_cand/consulta_cand_2016_PR.txt'
+DONATIONS_PATH = CURRENT_PATH + \
+    '/databases/receitas_despesas/receitas_candidatos_prestacao_contas_final_2016_PR.txt'
+
+
+def read_candidates_file(context, file_path):
+    '''Read file with candidates info.
+    Parameters:
+    +context+ - spark context.
+    +file_path+ - path to the candidates file.
+    '''
+    candidates_file = context.textFile(file_path)
+    candidates_splitted_in_parts = candidates_file.map(
+        lambda line: line.replace('"', '').split(';')
+    )
+    return candidates_splitted_in_parts.map(
+        lambda candidate: Row(
+            id=candidate[13], # CPF
+            cod_cidade=int(candidate[6]), cargo=candidate[9],
+            nome=candidate[10], num_cand=int(candidate[12]),
+            cidade=candidate[7], cod_status=int(candidate[43]),
+            status=candidate[44], partido=int(candidate[17]),
+            nasc=candidate[26], genero=candidate[30]
+        )
+    ).toDF()
+
+def read_donations_file(context, file_path):
+    '''Read file with donations to candidates
+    Parameters:
+    +context+ - spark context.
+    +file_path+ - path to the donations file.
+    '''
+    donations_file = context.textFile(file_path)
+    donations_splitted_in_parts = donations_file.map(
+        lambda line: line.replace('"', '').replace(',', '.').split(';')
+    )
+    return donations_splitted_in_parts.map(
+        lambda donation: Row(
+            dst=donation[12],
+            src=donation[16],
+            nome_doador=donation[17],
+            cod_cidade=int(donation[6]),
+            valor=float(donation[25]),
+            descricao=donation[29]
+        )
+    ).toDF()
+
+def main():
+    spark_context = SparkContext()
+    sql_context = SQLContext(spark_context)
+
+    candidates = read_candidates_file(spark_context, CANDIDATES_PATH)
+    donations = read_donations_file(spark_context, DONATIONS_PATH)
+    print donations.take(10)
+
+
+if __name__ == '__main__':
+   main()
