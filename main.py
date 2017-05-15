0148a4e7569ba3f4e2a227102ac1aa07cb17e330
Matheus Nerone <matheus.agio@hotmail.com>
2017-05-15 13:53:17 -0300

    Maybe finished

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 '''This script creates a social network using candidates as vertices and donations as edges
 '''
 import os
+from itertools import chain
 from pyspark import SparkContext, SQLContext
 from pyspark.sql import Row
 import pyspark.sql.functions as func
+from pyspark.sql.types import StringType, ArrayType
 from graphframes import *
 from local_clustering_coefficient import LocalClusteringCoefficient
 from assortativity import Assortativity
@@ -64,13 +66,18 @@ def clean_graph(graph):
     - Removing self reference edges.
     - Removing multiple edges from the same src to the same dst.
     - Removing duplication of ID's in the vertices.
-    - Adding srcs and dsts not in vertices to vertices.
+    - Adding srcs and dsts not in vertices into vertices.
+    - Removing edges where both dst and src are not in the vertices.
 
     '''
+    print 'Graph cleaning'
+    print ' - Removing ID duplication'
     vertices = graph.vertices.select('id').distinct()
+    print ' - Removing multiple connections'
     edges = graph.edges.where('src != dst').select('src', 'dst').distinct()
-    ids = vertices.rdd.map(lambda r: r['id']).collect()
 
+    print ' - Adding srcs and dsts that are not in vertices into vertices'
+    ids = vertices.rdd.map(lambda r: r['id']).collect()
     srcs_not_in = edges.where((edges.src.isin(ids) == False) & (edges.dst.isin(ids)))
     srcs_not_in = srcs_not_in.select(srcs_not_in.src.alias('id')).distinct()
     dsts_not_in = edges.where((edges.dst.isin(ids) == False) & (edges.src.isin(ids)))
@@ -78,6 +85,10 @@ def clean_graph(graph):
 
     vertices = vertices.unionAll(srcs_not_in)
     vertices = vertices.unionAll(dsts_not_in)
+
+    print ' - Removing edges where both dst and src are not in the vertices'
+    ids = vertices.rdd.map(lambda r: r['id']).collect()
+    edges = edges.where((edges.src.isin(ids)) & (edges.dst.isin(ids)))
     return GraphFrame(vertices, edges)
 
 
@@ -92,6 +103,123 @@ def average_shortest_path(graph):
     results = graph.shortestPaths(landmarks=vertices_sample)
     return results.select('id', func.explode('distances').alias('key', 'value'))\
                   .groupBy().agg(func.avg('value').alias('average')).collect()[0]['average']
+
+
+def biggest_connected_component(graph):
+    '''Return the biggest connected component
+    '''
+    return graph.connectedComponents(algorithm='graphx').groupBy('component').agg(
+        func.countDistinct('id').alias('size')
+    ).sort(func.desc('size')).rdd.take(1)[0]
+
+
+def biggest_strong_connected_component(graph):
+    '''Return the biggest strong connected component
+    '''
+    return graph.connectedComponents(maxIter=10).groupBy('component').agg(
+        func.countDistinct('id').alias('size')
+    ).sort(func.desc('size')).rdd.take(1)[0]
+
+
+def largest_wcc(graph):
+    '''Returns the percentage that the biggest connect component covers
+    '''
+    print 'Calculating largest connect component coverage'
+    print ' - Calculating largest connect component size'
+    size = biggest_connected_component(graph)['size']
+    print ' - Calculating number of vertices in graph'
+    return (size / float(n_of_vertices(graph))) * 100
+
+
+def largest_scc(graph):
+    '''Returns the percentage that the biggest strong connect component covers
+    '''
+    print 'Calculating largest strong connect component coverage'
+    print ' - Calculating largest strong connect component size'
+    size = biggest_strong_connected_component(graph)['size']
+    print ' - Calculating number of vertices in graph'
+    return (size / float(n_of_vertices(graph))) * 100
+
+
+def my_chain(*args):
+    '''Transform None to [] before chaining them
+    '''
+    n_args = []
+    for arg in args:
+        if arg is None:
+            n_args.append([])
+        else:
+            n_args.append(arg)
+    return chain(*tuple(n_args))
+
+
+def concat(defined_type):
+    '''UDF func to concat arrays
+    '''
+    def concat_(*args):
+        '''Concat arrays and eliminates duplicates
+        '''
+        return list(set(my_chain(*args)))
+    return func.udf(concat_, ArrayType(defined_type))
+
+
+def average_degree(graph):
+    '''Return the average degree of the graph
+    '''
+    print 'Calculating Average Degree'
+    vertices = graph.vertices
+    edges = graph.edges.where('src != dst')
+
+    print ' - Finding out neighbors'
+    out_neighbors_ids = edges.select(edges.src.alias('id'), 'dst').distinct().groupBy(
+        'id').agg(func.expr('collect_list(dst) AS out_neighbors'))
+
+    out_neighbors_ids = vertices.join(
+        out_neighbors_ids, out_neighbors_ids.id == vertices.id, 'left_outer'
+    ).select(
+        vertices.id,
+        out_neighbors_ids.out_neighbors.alias('out_neighbors')
+    )
+
+    print ' - Finding in neighbors'
+    in_neighbors_ids = edges.select(edges.dst.alias('id'), 'src').distinct().groupBy(
+        'id').agg(func.expr('collect_list(src) AS in_neighbors'))
+
+    in_neighbors_ids = vertices.join(
+        in_neighbors_ids, in_neighbors_ids.id == vertices.id, 'left_outer'
+    ).select(
+        vertices.id,
+        in_neighbors_ids.in_neighbors.alias('in_neighbors')
+    )
+
+    print ' - Joining them'
+    concat_string_arrays = concat(StringType())
+    neighbors_ids = out_neighbors_ids.join(
+        in_neighbors_ids, out_neighbors_ids.id == in_neighbors_ids.id
+    ).select(
+        in_neighbors_ids.id,
+        concat_string_arrays(
+            'in_neighbors', 'out_neighbors').alias('neighbors')
+    )
+
+    print ' - Counting neighbors'
+    n_of_neighbors = neighbors_ids.select(
+        'id', func.size('neighbors').alias('n_of_neighbors'))
+    return n_of_neighbors.agg(
+        func.avg('n_of_neighbors').alias('avg')
+    ).take(1)[0]['avg']
+
+
+def n_of_vertices(graph):
+    '''Return the number of vertices in graph
+    '''
+    return graph.vertices.count()
+
+
+def n_of_edges(graph):
+    '''Return the number of edges in graph
+    '''
+    return graph.edges.count()
 
 
 def print_result(result):
@@ -115,16 +243,42 @@ def main():
     print 'Reading donations file'
     donations = read_donations_file(spark_context, DONATIONS_PATH)
 
-    print 'Build graph'
+    print 'Building graph'
 
     graph = clean_graph(GraphFrame(candidates, donations))
 
-    print_result(Assortativity(graph).calculate())
+    number_of_vertices = n_of_vertices(graph)
+    print 'Number of vertices: ', number_of_vertices
 
-    print_result(LocalClusteringCoefficient(graph).calculate_average())
+    number_of_edges = n_of_edges(graph)
+    print 'Number of edges: ', number_of_edges
 
-    print_result(average_shortest_path(graph))
+    avg_degree = average_degree(graph)
+    print_result(avg_degree)
 
+    assortativity = Assortativity(graph).calculate()
+    print_result(assortativity)
+
+    lcc = LocalClusteringCoefficient(graph).calculate_average()
+    print_result(lcc)
+
+    avg_shortest_path = average_shortest_path(graph)
+    print_result(avg_shortest_path)
+
+    wcc = largest_wcc(graph)
+    print_result(wcc)
+
+    scc = largest_scc(graph)
+    print_result(scc)
+
+    print '@@@@@@@@@@@@@@@@@@@@ FINAL RESULT @@@@@@@@@@@@@@@@@@@@@@@'
+    schema = ["N of nodes", "N of edges", "Avg. Degree", "Clustering Coef.",
+              "Avg. Path Length", "Assorativity Coef.", "Largest SCC", "Largest WCC"]
+    data = (number_of_vertices, number_of_edges, avg_degree, lcc,\
+            avg_shortest_path, assortativity, scc, wcc)
+    result = sql_context.createDataFrame([data], schema)
+    result.show()
+    print '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
     print '******************** FINISHING SCRIPT *******************'
     print '*********************************************************'
 
